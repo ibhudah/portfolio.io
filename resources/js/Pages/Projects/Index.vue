da8ae2f100e64df75c5e87c8860cb0efd88f0815
black-hawksec <mthuthuzeli@black-hawksec.co.za>
2022-11-07 04:12:13 +0200

    first commit

---
+++ resources/js/Pages/Projects/Index.vue
@@ -0,0 +1,28 @@
+<template>
+    <Head title="Projects Index" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                New Project
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-end m-2 p-2">
+                    <Link href="/projects/create" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white round-md">New Projet</Link>
+                </div> 
+            </div>
+                
+            
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+    import { Head, Link } from '@inertiajs/inertia-vue3';
+
+</script>
+
