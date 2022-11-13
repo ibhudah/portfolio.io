700475b19b4a623e82f48879853264da8a8bda93
black-hawksec <mthuthuzeli@black-hawksec.co.za>
2022-11-13 02:25:09 +0200

    Skill upload

--- database/migrations/2022_11_03_124740_create_skills_table.php
+++ database/migrations/2022_11_03_124740_create_skills_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
-            $table->string('mame');
+            $table->string('name');
             $table->string('image');
             $table->timestamps();
         });
